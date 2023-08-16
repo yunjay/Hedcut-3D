f0cc01d9c8aeedcbbe7d3950fe9c7b5be3d235a5
Yun Jay Kim <yunjaysus@gmail.com>
2023-08-17 08:41:22 +0900

    Init Texture.h

--- source/Texture.h
+++ source/Texture.h
@@ -1,1 +1,21 @@
 #pragma once
+class Texture{
+public:
+  Texture(int width, int height, GLenum format):
+    m_width{width},m_height{height},m_format{format}
+  {
+    
+  }
+  int GetWidth() const {
+		return m_width;
+	}
+	int GetHeight() const {
+		return m_height;
+	}
+	GLenum GetImageFormat() const {
+		return m_format;
+	}
+private:
+  int m_width, m_height;
+  GLenum m_format;
+}
