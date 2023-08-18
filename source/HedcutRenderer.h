b76d4afb5d111a1e7e92deb3065a57ff5b9f9a39
Yun Jay Kim <yunjaysus@gmail.com>
2023-08-18 10:57:58 +0900

    Update HedcutRenderer.h

--- source/HedcutRenderer.h
+++ source/HedcutRenderer.h
@@ -1,31 +1,13 @@
 #pragma once
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <GL/glew.h>
-
-using glm::vec3; using glm::vec4; using glm::vec2;
-using glm::dot; using glm::cross; using glm::normalize; using glm::sign;
+class Framebuffer; class Camera; class Mesh; class Texture;
 class HedcutRenderer {
 public:
 // I'd rather not get into coding a scene graph / scene class.
 // I'll stick to one mesh at a time.
 //	static void Render(GLuint fbo, Scene scene, Camera camera) {}
-	static void Render(GLuint fbo, Mesh mesh, Camera camera){
-		// Bind fbo
-		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-
-
-	}
-	static void Render(Mesh mesh, Camera camera) {
-		// 0 : default framebuffer
-		Render(0, scene, camera);
-	}
-	static void RenderToImage(Mesh mesh, Camera camera) {
-		Framebuffer fboToTexture;
-		Texture renderTargetTexture;
-		Render(fboToTexture, mesh, camera);
-	}
+	static void Render(GLuint fbo, Mesh mesh, Camera camera);
+	static void Render(Mesh mesh, Camera camera);
+	static void RenderToImage(Mesh mesh, Camera camera);
 private:
 static Framebuffer m_GbufferFramebuffer;
 static Texture m_PositionTexture, m_NormalTexture, m_PrincipalDirectionTexture, m_AlbedoSpecular, m_DepthTexture;
